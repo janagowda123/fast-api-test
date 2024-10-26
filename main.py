7ba1e4ca351b4b70f25db91376fb74d5267ea691
Jana Gowda <janagowda@gmail.com>
2024-10-26 17:09:56 +0530

    first commi 5t

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ async def read_item(item_id: str):
     
     return {"item_id": item_id,"Hello":"yrutjt"}
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
